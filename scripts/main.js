39389575b20ed26ffb44cbc9c614a9b63e33242c
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2022-12-15 07:03:08 +0300

    prettierConfig

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 // PROFILE
-const popupProfile = document.querySelector("#popupProfile")
-const profileForm = document.querySelector("#profileForm")
-const profileClose = document.querySelector("#profileClose")
-const profileName = document.querySelector(".profile__name")
-const profileDescription = document.querySelector(".profile__description")
-const profileEdit = document.querySelector(".profile__edit")
+const popupProfile = document.querySelector('#popupProfile')
+const profileForm = document.querySelector('#profileForm')
+const profileClose = document.querySelector('#profileClose')
+const profileName = document.querySelector('.profile__name')
+const profileDescription = document.querySelector('.profile__description')
+const profileEdit = document.querySelector('.profile__edit')
 const profileAdd = document.querySelector('.profile__add')
-const newProfileName = document.querySelector("#profileName")
-const newProfileDescription = document.querySelector("#profileDescription")
+const newProfileName = document.querySelector('#profileName')
+const newProfileDescription = document.querySelector('#profileDescription')
 const popups = Array.from(document.querySelectorAll('.popup'))
 const popupCreate = document.querySelector('#popupCreate')
 // EDIT
@@ -29,112 +29,112 @@ const imageText = document.querySelector('.popup__text')
 
 // Открыть popup
 function openPopup(popup) {
-  popup.classList.add("popup_opened")
-  // Вызов слушателя по нажатию на Escape
-  document.addEventListener('keydown', closeOnEsc)
-  // Закрытие по нажатию overlay
-  popups.forEach((popup) => {
-    popup.addEventListener('click', closeOnOverlay)
-  })
+	popup.classList.add('popup_opened')
+	// Вызов слушателя по нажатию на Escape
+	document.addEventListener('keydown', closeOnEsc)
+	// Закрытие по нажатию overlay
+	popups.forEach((popup) => {
+		popup.addEventListener('click', closeOnOverlay)
+	})
 }
 // Закрытие popup по нажатию на Escape
 function closeOnEsc(evt) {
-  if (evt.key === 'Escape') {
-    const popupOpened = document.querySelector(".popup_opened")
-    closePopup(popupOpened)
-  }
+	if (evt.key === 'Escape') {
+		const popupOpened = document.querySelector('.popup_opened')
+		closePopup(popupOpened)
+	}
 }
 // Закрытие popup по нажатию на overlay
 function closeOnOverlay(evt) {
-  if (evt.target.classList.contains('popup')) {
-    closePopup(evt.target)
-  }
+	if (evt.target.classList.contains('popup')) {
+		closePopup(evt.target)
+	}
 }
 // Закрыть popup
 function closePopup(popup) {
-  popup.classList.remove("popup_opened")
-  document.removeEventListener('keydown', closeOnEsc)
+	popup.classList.remove('popup_opened')
+	document.removeEventListener('keydown', closeOnEsc)
 }
 // Заполнить данные профиля
 function setProfileData() {
-  newProfileName.value = profileName.textContent
-  newProfileDescription.value = profileDescription.textContent
-  openPopup(popupProfile)
+	newProfileName.value = profileName.textContent
+	newProfileDescription.value = profileDescription.textContent
+	openPopup(popupProfile)
 }
 // Сохранить данные профиля
 function saveProfileData() {
-  profileName.textContent = newProfileName.value
-  profileDescription.textContent = newProfileDescription.value
-  closePopup(popupProfile)
+	profileName.textContent = newProfileName.value
+	profileDescription.textContent = newProfileDescription.value
+	closePopup(popupProfile)
 }
 // Отключить кнопку submit
 
 // Отобразить карточки
 function displayElement(card) {
-  const element = elementTemplate.querySelector('.elements__card').cloneNode(true)
-  const elementsTitle = element.querySelector('.elements__title')
-  const elementsImage = element.querySelector('.elements__image')
+	const element = elementTemplate.querySelector('.elements__card').cloneNode(true)
+	const elementsTitle = element.querySelector('.elements__title')
+	const elementsImage = element.querySelector('.elements__image')
 
-  elementsImage.src = card.link
-  elementsImage.alt = card.name
-  elementsTitle.textContent = card.name
+	elementsImage.src = card.link
+	elementsImage.alt = card.name
+	elementsTitle.textContent = card.name
 
-  element.querySelector('.elements__image').addEventListener('click', openPopupImage)
+	element.querySelector('.elements__image').addEventListener('click', openPopupImage)
 
-  element.querySelector('.elements__like').addEventListener('click', (evt) => {
-    evt.target.classList.toggle('elements__like_active')
-  })
+	element.querySelector('.elements__like').addEventListener('click', (evt) => {
+		evt.target.classList.toggle('elements__like_active')
+	})
 
-  element.querySelector('.elements__trash').addEventListener('click', (evt) => {
-    evt.target.closest('.elements__card').remove()
-  })
-  // Функция открытия popup картинки
-  function openPopupImage() {
-    imageElement.src = elementsImage.src
-    imageElement.alt = elementsImage.alt
-    imageText.textContent = elementsTitle.textContent
-    openPopup(popupImage)
-  }
+	element.querySelector('.elements__trash').addEventListener('click', (evt) => {
+		evt.target.closest('.elements__card').remove()
+	})
+	// Функция открытия popup картинки
+	function openPopupImage() {
+		imageElement.src = elementsImage.src
+		imageElement.alt = elementsImage.alt
+		imageText.textContent = elementsTitle.textContent
+		openPopup(popupImage)
+	}
 
-  return element
+	return element
 }
 // Вставить карточки
 function addElement(card, container) {
-  container.prepend(card)
+	container.prepend(card)
 }
 // Создать карточки
 function createElement() {
-  const newElementData = {
-    name: elementTitle.value,
-    link: elementImage.value
-  }
+	const newElementData = {
+		name: elementTitle.value,
+		link: elementImage.value,
+	}
 
-  const newElement = displayElement(newElementData)
-  addElement(newElement, elementsContainer)
+	const newElement = displayElement(newElementData)
+	addElement(newElement, elementsContainer)
 
-  disableSumbitButton(popupCreate)
-  cardForm.reset()
+	disableSumbitButton(popupCreate)
+	cardForm.reset()
 
-  closePopup(popupEdit)
+	closePopup(popupEdit)
 }
 
-initialCards.forEach(elementData => {
-  const element = displayElement(elementData)
-  addElement(element, elementsContainer)
+initialCards.forEach((elementData) => {
+	const element = displayElement(elementData)
+	addElement(element, elementsContainer)
 })
 
-profileForm.addEventListener("submit", (evt) => {
-  evt.preventDefault()
-  saveProfileData()
+profileForm.addEventListener('submit', (evt) => {
+	evt.preventDefault()
+	saveProfileData()
 })
 
 cardForm.addEventListener('submit', (evt) => {
-  evt.preventDefault()
-  createElement()
+	evt.preventDefault()
+	createElement()
 })
 
-profileEdit.addEventListener("click", setProfileData)
-profileClose.addEventListener("click", () => closePopup(popupProfile))
+profileEdit.addEventListener('click', setProfileData)
+profileClose.addEventListener('click', () => closePopup(popupProfile))
 profileAdd.addEventListener('click', () => openPopup(popupEdit))
 cardClose.addEventListener('click', () => closePopup(popupEdit))
 imageClose.addEventListener('click', () => closePopup(popupImage))
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,81 +1,81 @@
 const validationProperties = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__save',
-  inactiveButtonClass: 'popup__save_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+	formSelector: '.popup__form',
+	inputSelector: '.popup__input',
+	submitButtonSelector: '.popup__save',
+	inactiveButtonClass: 'popup__save_disabled',
+	inputErrorClass: 'popup__input_type_error',
+	errorClass: 'popup__error_visible',
 }
 // Отобразить ошибки в полях ввода
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.add(validationProperties.inputErrorClass)
-  errorElement.textContent = errorMessage
-  errorElement.classList.add(validationProperties.errorClass)
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+	inputElement.classList.add(validationProperties.inputErrorClass)
+	errorElement.textContent = errorMessage
+	errorElement.classList.add(validationProperties.errorClass)
 }
 // Спрятать ошибки в полях ввода
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.remove(validationProperties.inputErrorClass)
-  errorElement.classList.remove(validationProperties.errorClass)
-  errorElement.textContent = ''
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+	inputElement.classList.remove(validationProperties.inputErrorClass)
+	errorElement.classList.remove(validationProperties.errorClass)
+	errorElement.textContent = ''
 }
 // Проверить поле ввода на валидность
 const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage)
-  } else {
-    hideInputError(formElement, inputElement)
-  }
+	if (!inputElement.validity.valid) {
+		showInputError(formElement, inputElement, inputElement.validationMessage)
+	} else {
+		hideInputError(formElement, inputElement)
+	}
 }
 
 const setEventListeners = (formElement, validationProperties) => {
-  const inputList = Array.from(formElement.querySelectorAll(validationProperties.inputSelector))
-  const buttonElement = formElement.querySelector(validationProperties.submitButtonSelector)
+	const inputList = Array.from(formElement.querySelectorAll(validationProperties.inputSelector))
+	const buttonElement = formElement.querySelector(validationProperties.submitButtonSelector)
 
-  toggleButtonState(inputList, buttonElement)
+	toggleButtonState(inputList, buttonElement)
 
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement)
-      toggleButtonState(inputList, buttonElement, validationProperties.inactiveButtonClass)
-    })
-  })
+	inputList.forEach((inputElement) => {
+		inputElement.addEventListener('input', function () {
+			checkInputValidity(formElement, inputElement)
+			toggleButtonState(inputList, buttonElement, validationProperties.inactiveButtonClass)
+		})
+	})
 }
 const enableValidation = (validationProperties) => {
-  const formList = Array.from(document.querySelectorAll(validationProperties.formSelector))
+	const formList = Array.from(document.querySelectorAll(validationProperties.formSelector))
 
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault()
-    })
-    setEventListeners(formElement, validationProperties)
-  })
+	formList.forEach((formElement) => {
+		formElement.addEventListener('submit', function (evt) {
+			evt.preventDefault()
+		})
+		setEventListeners(formElement, validationProperties)
+	})
 }
 // Проверить отсутствие валидации
 function hasInvalidInput(inputList) {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid
-  })
+	return inputList.some((inputElement) => {
+		return !inputElement.validity.valid
+	})
 }
 
 function disableSumbitButton(buttonElement) {
-  buttonElement.disabled = true
-  buttonElement.classList.add(validationProperties.inactiveButtonClass)
+	buttonElement.disabled = true
+	buttonElement.classList.add(validationProperties.inactiveButtonClass)
 }
 
 function enableSumbitButton(buttonElement) {
-  buttonElement.disabled = false
-  buttonElement.classList.remove(validationProperties.inactiveButtonClass);
+	buttonElement.disabled = false
+	buttonElement.classList.remove(validationProperties.inactiveButtonClass)
 }
 
 // Переключение статуса кнопки submit
 function toggleButtonState(inputList, buttonElement) {
-  if (hasInvalidInput(inputList)) {
-    disableSumbitButton(buttonElement)
-  } else {
-    enableSumbitButton(buttonElement)
-  }
+	if (hasInvalidInput(inputList)) {
+		disableSumbitButton(buttonElement)
+	} else {
+		enableSumbitButton(buttonElement)
+	}
 }
 
 enableValidation(validationProperties)
